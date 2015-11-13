7e1cda4626ac6f5806de3285a2c48c89de29191c
Jason Vasquez <jason.vasquez@developertown.com>
2015-11-13 09:12:06 -0500

    Only activate ssh agent if we have a identity (e.g., mounted the .ssh volume)

--- .zsh.d/ssh-agent.zsh
+++ .zsh.d/ssh-agent.zsh
@@ -1,2 +1,4 @@
-source $ZSH/plugins/ssh-agent/ssh-agent.plugin.zsh
-zstyle :omz:plugins:ssh-agent agent-forwarding on
+if [ -f $HOME/.ssh/id_rsa ]; then
+  source $ZSH/plugins/ssh-agent/ssh-agent.plugin.zsh
+  zstyle :omz:plugins:ssh-agent agent-forwarding on
+fi
